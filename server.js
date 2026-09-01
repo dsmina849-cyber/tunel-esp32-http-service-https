af6f3b0f293f0288fac9b6adefbb122393d76adf
dsmina849-cyber <dsmina849@gmail.com>
2026-08-31 23:04:06 -0500

    Update server.js

--- server.js
+++ server.js
@@ -35,5 +35,5 @@ app.get('*', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(Servidor puente corriendo en el puerto ${PORT});
+    console.log('Servidor puente corriendo en el puerto ' + PORT);
 });
